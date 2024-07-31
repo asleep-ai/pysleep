6f1e937bf79fb09231e7a0fb9208215e0f223ddc
Hankyeol Kyung <kghnkl0103@gmail.com>
2024-07-31 22:03:44 +0900

    Add package definition and basic labels for first release (#1)
    * Define package
    
    * Add gitignore
    
    Signed-off-by: Hankyeol Kyung <kghnkl0103@gmail.com>
    
    * Add basic labels
    
    Signed-off-by: Hankyeol Kyung <kghnkl0103@gmail.com>
    
    ---------
    
    Signed-off-by: Hankyeol Kyung <kghnkl0103@gmail.com>

---
+++ src/pysleep/__init__.py
@@ -0,0 +1,14 @@
+"""
+pysleep
+=====
+
+Provides
+  1. Definition of the sleep in python ecosystem
+  2. Metrics used in medical field
+"""
+
+__version__ = "0.0.1"
+
+from .label import APNEA, DEEP, HYPOPNEA, LIGHT, NO_EVENT, REM, SNORE, WAKE
+
+__all__ = ["APNEA", "DEEP", "HYPOPNEA", "LIGHT", "NO_EVENT", "REM", "SNORE", "WAKE"]
