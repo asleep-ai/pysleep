1078e9194d4aee6215c8a688d1eb91f9a51006dd
Hankyeol Kyung <kghnkl0103@gmail.com>
2024-11-08 17:51:36 +0900

    Bump version to 0.1.0
    Signed-off-by: Hankyeol Kyung <kghnkl0103@gmail.com>

--- src/pysleep/__init__.py
+++ src/pysleep/__init__.py
@@ -7,7 +7,7 @@ Provides
   2. Metrics used in medical field
 """
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 from .label import APNEA, DEEP, HYPOPNEA, LIGHT, NO_EVENT, REM, SNORE, WAKE
 from .stat import SleepStat, SleepStatDelta
