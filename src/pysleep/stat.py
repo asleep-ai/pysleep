95d27d1b70381c007c073ad0a4e44371ffa4d2b1
Hankyeol Kyung <kghnkl0103@gmail.com>
2024-11-14 11:28:56 +0900

    Refactor SleepStatDelta subtraction logic for improved clarity and consistency
    Signed-off-by: Hankyeol Kyung <kghnkl0103@gmail.com>

--- src/pysleep/stat.py
+++ src/pysleep/stat.py
@@ -49,6 +49,12 @@ class SleepStat:
         self.sleep_cycle_time = [t.astimezone(tz) for t in self.sleep_cycle_time]
 
     def __sub__(self, other: "SleepStat") -> "SleepStatDelta":
+        """
+        Calculate the difference between two SleepStat instances.
+
+        Args:
+            other (SleepStat): The other SleepStat instance to subtract from this instance.
+        """
         return SleepStatDelta(
             sleep_time=subtract_relative_time(self.sleep_time, other.sleep_time),
             wake_time=subtract_relative_time(self.wake_time, other.wake_time),
@@ -67,45 +73,34 @@ class SleepStat:
             time_in_rem=self.time_in_rem - other.time_in_rem,
             time_in_stable_breath=self.time_in_stable_breath - other.time_in_stable_breath,
             time_in_unstable_breath=self.time_in_unstable_breath - other.time_in_unstable_breath,
-            time_in_snoring=self.time_in_snoring - other.time_in_snoring
-            if self.time_in_snoring and other.time_in_snoring
-            else None,
-            time_in_no_snoring=self.time_in_no_snoring - other.time_in_no_snoring
-            if self.time_in_no_snoring and other.time_in_no_snoring
-            else None,
+            time_in_snoring=subtract_if_not_none(self.time_in_snoring, other.time_in_snoring),
+            time_in_no_snoring=subtract_if_not_none(self.time_in_no_snoring, other.time_in_no_snoring),
             sleep_efficiency=self.sleep_efficiency - other.sleep_efficiency,
-            sleep_ratio=self.sleep_ratio - other.sleep_ratio if self.sleep_ratio and other.sleep_ratio else None,
-            wake_ratio=self.wake_ratio - other.wake_ratio if self.wake_ratio and other.wake_ratio else None,
-            light_ratio=self.light_ratio - other.light_ratio if self.light_ratio and other.light_ratio else None,
-            deep_ratio=self.deep_ratio - other.deep_ratio if self.deep_ratio and other.deep_ratio else None,
-            rem_ratio=self.rem_ratio - other.rem_ratio if self.rem_ratio and other.rem_ratio else None,
-            stable_breath_ratio=self.stable_breath_ratio - other.stable_breath_ratio
-            if self.stable_breath_ratio and other.stable_breath_ratio
-            else None,
-            unstable_breath_ratio=self.unstable_breath_ratio - other.unstable_breath_ratio
-            if self.unstable_breath_ratio and other.unstable_breath_ratio
-            else None,
-            snoring_ratio=self.snoring_ratio - other.snoring_ratio
-            if self.snoring_ratio and other.snoring_ratio
-            else None,
-            no_snoring_ratio=self.no_snoring_ratio - other.no_snoring_ratio
-            if self.no_snoring_ratio and other.no_snoring_ratio
-            else None,
+            sleep_ratio=subtract_if_not_none(self.sleep_ratio, other.sleep_ratio),
+            wake_ratio=subtract_if_not_none(self.wake_ratio, other.wake_ratio),
+            light_ratio=subtract_if_not_none(self.light_ratio, other.light_ratio),
+            deep_ratio=subtract_if_not_none(self.deep_ratio, other.deep_ratio),
+            rem_ratio=subtract_if_not_none(self.rem_ratio, other.rem_ratio),
+            stable_breath_ratio=subtract_if_not_none(self.stable_breath_ratio, other.stable_breath_ratio),
+            unstable_breath_ratio=subtract_if_not_none(self.unstable_breath_ratio, other.unstable_breath_ratio),
+            snoring_ratio=subtract_if_not_none(self.snoring_ratio, other.snoring_ratio),
+            no_snoring_ratio=subtract_if_not_none(self.no_snoring_ratio, other.no_snoring_ratio),
             breathing_index=self.breathing_index - other.breathing_index,
-            waso_count=self.waso_count - other.waso_count if self.waso_count and other.waso_count else None,
-            longest_waso=self.longest_waso - other.longest_waso if self.longest_waso and other.longest_waso else None,
-            sleep_cycle_count=self.sleep_cycle_count - other.sleep_cycle_count
-            if self.sleep_cycle_count and other.sleep_cycle_count
-            else None,
-            sleep_cycle=self.sleep_cycle - other.sleep_cycle if self.sleep_cycle and other.sleep_cycle else None,
-            unstable_breath_count=self.unstable_breath_count - other.unstable_breath_count
-            if self.unstable_breath_count and other.unstable_breath_count
-            else None,
-            snoring_count=self.snoring_count - other.snoring_count if self.snoring_count and other.snoring_count else None 
+            waso_count=subtract_if_not_none(self.waso_count, other.waso_count),
+            longest_waso=subtract_if_not_none(self.longest_waso, other.longest_waso),
+            sleep_cycle_count=subtract_if_not_none(self.sleep_cycle_count, other.sleep_cycle_count),
+            sleep_cycle=subtract_if_not_none(self.sleep_cycle, other.sleep_cycle),
+            unstable_breath_count=subtract_if_not_none(self.unstable_breath_count, other.unstable_breath_count),
+            snoring_count=subtract_if_not_none(self.snoring_count, other.snoring_count),
         )
 
 
 class SleepStatDelta:
+    """
+    A class to represent the difference between two SleepStat instances.
+    It contains the differences in various sleep metrics.
+    """
+
     def __init__(
         self,
         sleep_time: timedelta,
@@ -181,6 +176,12 @@ class SleepStatDelta:
         self.sleep_cycle = sleep_cycle
         self.unstable_breath_count = unstable_breath_count
         self.snoring_count = snoring_count
+
+
+def subtract_if_not_none(a, b):
+    if a is not None and b is not None:
+        return a - b
+    return None
 
 
 def subtract_relative_time(dt1: datetime, dt2: datetime) -> timedelta:
